149284788770cbfa98d6a19d112360461e9408bd
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2023-10-03 20:32:09 -0700

    add model download

--- model_invoke.py
+++ model_invoke.py
@@ -1,11 +1,13 @@
 from diffusers import StableDiffusionPipeline
+from huggingface_hub import hf_hub_download
 import torch
 import json
 
-pipe = StableDiffusionPipeline.from_pretrained("Justin-Choo/epiCRealism-Natural_Sin_RC1_VAE",
-                                                cache_dir="./.cache")
+model = hf_hub_download(repo_id="Remilistrasza/ClustroAI_LoRA", filename="ghibli_style_offset.safetensors")
+pipe = StableDiffusionPipeline.from_single_file(model,
+                                                torch_dtype=torch.float16, 
+                                                safety_checker=None)
 pipe = pipe.to("cuda")
-pipe.safety_checker = None
 
 def invoke(input_text):
     input_json = json.loads(input_text)
@@ -15,10 +17,3 @@ def invoke(input_text):
     image = pipe(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=steps).images[0]
     image.save("generated_image.png")
     return "generated_image.png"
-
-invoke(
-    "{\"prompt\":\"polaroid photo, night photo, photo of 24 y.o beautiful woman, pale skin, bokeh, motion blur\"," + 
-    "\"negative_prompt\":\"(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime, mutated " + 
-    "hands and fingers:1.4), (deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, " + 
-    "extra limb, missing limb, floating limbs, disconnected limbs, mutation, mutated, ugly, disgusting, amputation\"}"
-    )
